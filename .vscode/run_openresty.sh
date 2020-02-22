8433df373af23db3e436bcaa996ee907ea5ebff1
林健 <linjian01@fangdd.com>
2020-02-22 21:49:48 +0800

    添加测试conf

--- .vscode/run_openresty.sh
+++ .vscode/run_openresty.sh
@@ -3,12 +3,12 @@ echo "重新启动本地 openresty 服务...";
 
 if [ -e "$@/logs/nginx.pid" ]; then
   echo "openresty 正在运行 --> 重启openresty";
-  sudo /usr/local/openresty/bin/openresty -p "$@" -c "$@/conf/nginx.conf" -s reload
+  sudo /usr/local/openresty/bin/openresty -p "$@" -c "$@/conf/nginx.test.conf" -s reload
   # ps -ef | grep opnresty
   echo "--> 重启完毕";
 else 
   echo "opnresty 未运行 --> 启动openresty";
-  sudo /usr/local/openresty/bin/openresty -p "$@" -c "$@/conf/nginx.conf"
+  sudo /usr/local/openresty/bin/openresty -p "$@" -c "$@/conf/nginx.test.conf"
   # ps -ef | grep opnresty
   echo "--> 启动完毕";
 fi;
